79738d555cec950aa7e7e8a703768b8fd360f7d7
anon <hajjar.charles@icloud.com>
2019-10-28 11:44:09 +0100

    Initial Commit

---
+++ newAPPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newAPPS
+//
+//  Created by ano on 28/10/2019.
+//  Copyright © 2019 hajjar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
