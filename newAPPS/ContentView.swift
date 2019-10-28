d28f9cf3d1133d3472f13881ddfc15a248e72cf5
anon <hajjar.charles@icloud.com>
2019-10-28 12:06:28 +0100

    "save"

--- newAPPS/ContentView.swift
+++ newAPPS/ContentView.swift
@@ -8,14 +8,82 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            
+            // MARK: Header
+            
+            HStack {
+                Image("profile1")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading) {
+                    Text("Charles")
+                        .font(.headline)
+                    
+                    Text("Posté il y a 3 heures")
+                        .font(.subheadline)
+                }
+                
+                Spacer()
+                
+                Image(systemName: "paperplane")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.gray)
+            
+            }.padding() // La valeur du padding est fournie par defaut
+            
+            // MARK: Image
+            
+            Image("post1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            // MARK: Footer
+            
+            VStack(alignment: .leading) {
+                
+                HStack(spacing: 16) {
+                    Image(systemName: "heart")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.gray)
+                    
+                    Image(systemName: "bubble.left")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.gray)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "flag")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.gray)
+                }
+                
+                Text("23 likes")
+                    .font(.headline)
+                
+                Text("Ceci est une description du poste de plusieurs lignes. Nous souhaitons que ce texte soit coupé s'il contient plus de 2 lignes.")
+                    .lineLimit(3)
+                
+            }.padding()
+            
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.sizeThatFits)
     }
 }
+#endif
